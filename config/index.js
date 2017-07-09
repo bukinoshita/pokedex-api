3213f74b6dd6e24bc2ffc0f902052debf8e50e72
Bu Kinoshita <bukinoshita@gmail.com>
2017-07-09 15:19:49 -0400

    registration endpoint (https://github.com/bukinoshita/pokedex-api/issues/2)

--- config/index.js
+++ config/index.js
@@ -1,13 +1,34 @@
 module.exports = {
   development: {
     port: 3001,
-    host: 'http://localhost:3001'
+    host: 'http://localhost:3001',
+    tokenSecret: 'awesome-pokedex',
+    db: 'mongodb://localhost/pokedex-api',
+    mailgun: {
+      apiKey: POKEDEX_MAILGUN_APIKEY,
+      from: 'bukinoshita@gmail.com',
+      domain: 'pokedex-api.now.sh'
+    }
   },
   staging: {
-    host: 'http://pokedex-api-staging,now.sh'
+    host: 'http://pokedex-api-staging,now.sh',
+    tokenSecret: 'awesome-pokedex',
+    db: 'mongodb://localhost/pokedex-api',
+    mailgun: {
+      apiKey: POKEDEX_MAILGUN_APIKEY,
+      from: 'bukinoshita@gmail.com',
+      domain: 'pokedex-api.now.sh'
+    }
   },
   production: {
     port: 3001,
-    host: 'https://pokedex-api.now.sh'
+    host: 'https://pokedex-api.now.sh',
+    tokenSecret: 'awesome-pokedex',
+    db: 'mongodb://localhost/pokedex-api',
+    mailgun: {
+      apiKey: POKEDEX_MAILGUN_APIKEY,
+      from: 'bukinoshita@gmail.com',
+      domain: 'pokedex-api.now.sh'
+    }
   }
 }
--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const expressValidator = require('express-validator')
+const mongoose = require('mongoose')
 
 const env = process.env.NODE_ENV || 'development'
 const config = require('./config')[env]
+const connectDb = require('./lib/helpers/db')
 const routes = require('./lib/routes')
 
 const app = express()
+
+mongoose.Promise = global.Promise
+
+connectDb()
 
 app.config = config
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(logger('dev'))
+app.use(expressValidator())
 app.set('port', config.port || process.env.PORT)
 app.use(cors())
 app.options('*', cors())
@@ -22,7 +30,11 @@ app.options('*', cors())
 routes(app)
 
 app.listen(app.get('port'), () => {
-  console.log(`Pokedex server is listening on port ${app.get('port')}`)
+  console.log(
+    `Pokedex server is listening on port ${app.get(
+      'port'
+    )} (http://localhost:${app.get('port')})`
+  )
   console.log(`Environment ${app.get('env')}`)
 })
 
