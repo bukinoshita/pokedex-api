78a48f4734832f125eba16df235544c1c94336ba
Bu Kinoshita <bukinoshita@gmail.com>
2017-07-10 12:20:04 -0400

    set old tokens expired to true when user is verified (https://github.com/bukinoshita/pokedex-api/issues/4\)

--- config/index.js
+++ config/index.js
@@ -11,7 +11,8 @@ module.exports = {
     }
   },
   staging: {
-    host: 'http://pokedex-api-staging,now.sh',
+    port: 3001,
+    host: 'https://pokedex-api-staging.now.sh',
     tokenSecret: 'awesome-pokedex',
     db: 'mongodb://localhost/pokedex-api',
     mailgun: {
@@ -24,7 +25,8 @@ module.exports = {
     port: 3001,
     host: 'https://pokedex-api.now.sh',
     tokenSecret: 'awesome-pokedex',
-    db: 'mongodb://localhost/pokedex-api',
+    db: `mongodb://${process.env.DB_USER}:${process.env
+      .DB_PASSWORD}@ds151702.mlab.com:51702/pokedex-api`,
     mailgun: {
       apiKey: process.env.MAILGUN_APIKEY,
       from: process.env.MAILGUN_FROM,
